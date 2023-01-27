ea2f11f4bb6d5974118b2642bbada066260d6e39
basemelazab <basem.elazab2014@gmail.com>
2023-01-27 07:18:52 +0200

    Add project files.

---
+++ WebApiCore/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApiCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCore.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly APIDBContext aPIDBContext;
+        public EmployeeController(APIDBContext aPIDBContext)
+        {
+            this.aPIDBContext = aPIDBContext;
+        }
+        [HttpGet]
+        [Route("action")]
+        [Route("api/Employee/GetAllEmployees")]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var Employees = await aPIDBContext.Employee.ToListAsync();
+            return Ok(Employees);
+        }
+    }
+}
